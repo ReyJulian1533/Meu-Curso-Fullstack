b3a32122c12a197845745bd64f49a784c5c80f8d
Gabriel <gabrielrodrigues2594@gmail.com>
2024-02-19 17:25:54 -0300

    saas

---
+++ Módulo 02/07 - Interação com API/Aula 05/Exemplo 03/exemplo03.js
@@ -0,0 +1,13 @@
+async function resolvrer(){
+    let busca1 = await fetch("tarefas.json")
+    let tarefas = await busca1.json()
+
+
+    for(let x in tarefas){
+         document.body.innerHTML += `<h1> ${tarefas[x]} </h1>`
+    }
+   
+
+}
+
+resolvrer()
