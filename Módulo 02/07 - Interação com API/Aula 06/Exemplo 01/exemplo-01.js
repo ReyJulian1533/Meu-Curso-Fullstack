9a23ff91f209dee3788eb7f59f4343c552a96153
Gabriel <gabrielrodrigues2594@gmail.com>
2024-02-21 17:04:53 -0300

    ss

---
+++ Módulo 02/07 - Interação com API/Aula 06/Exemplo 01/exemplo-01.js
@@ -0,0 +1,19 @@
+async function concessionaria(){
+    let resultado = await fetch("carros.json")
+    let carros = await resultado.json()
+
+    
+   //alert("A marca é " + carros.marca + " o modelo é " + carros.modelo + " o ano é " + carros.ano + " e a cor é " + carros.cor + ".")
+   //alert(carros[1].modelo)
+
+  
+        document.body.innerHTML +=`
+        <p> A marca do carro é ${carros[1].marca}</p>
+        <p> A modelo do carro é ${carros[1].modelo}</p>
+        <p> A ano do carro é ${carros[1].ano}</p>
+        <p> A cor do carro é ${carros[1].cor}</p>`
+   
+}
+concessionaria()
+
+    //A marca é x, o modelo é x, o ano é x e a cor é x
