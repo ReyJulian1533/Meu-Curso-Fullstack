39f6d10331f48a1774503e99b472747ac81bdd4b
ReyJulian1533 <146767127+ReyJulian1533@users.noreply.github.com>
2024-03-27 17:21:18 -0300

    cc

--- Módulo 02/07 - Interação com API/Aula 06/Exemplo 05/detalhes.js
+++ Módulo 02/07 - Interação com API/Aula 06/Exemplo 05/detalhes.js
@@ -14,7 +14,7 @@ async function buscarDetalhes(){
 
     document.getElementById("detalhes").innerHTML += `
         <h3>${produtos[indicieProd].produto}</h3>
-        <img src="${produtos[indicieProd].imagem[0]}" width="250" height"250">
+        <img src="${produtos[indicieProd].imagem[0]}" id="frame" width="250" height"250" style="border: 1px solid #000">
         <div class="miniaturas" id="miniaturas">
 
         </div>
@@ -23,9 +23,18 @@ async function buscarDetalhes(){
     let divMiniaturas = document.getElementById("miniaturas")
     for(let x of produtos[indicieProd].imagem){
             divMiniaturas.innerHTML +=`
-                <img src="${x}" width="80" style="border: 1px solid #000">
+                <img src="${x}" class="mini" width="80" style="border: 1px solid #000">
             `
     }
+
+    let minizinhas = document.getElementsByClassName("mini")
+    for(let x of minizinhas){
+        x.addEventListener("mouseover", deslize)
+    }
+}
+
+function deslize(){
+    document.getElementById("frame").src = this.src
 }
 
 buscarDetalhes()
