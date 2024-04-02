c890ccf1c52070874d7aa909786342cbb91cc7f3
ReyJulian1533 <gabrielrodrigues2594@gmail.com>
2024-04-02 17:00:46 -0300

    cc

---
+++ Módulo 02/07 - Interação com API/Projeto/detalhes.js
@@ -0,0 +1,40 @@
+async function buscarDetalhes(){
+    let busca = await fetch("lista.json")
+    let produtos = await busca.json()
+
+    let parametros = new URLSearchParams(window.location.search)
+    let parametroID = parametros.get("produto-id")
+
+    let indicieProd
+    for(let x in produtos){
+        if(produtos[x].id == parametroID){
+            indicieProd = x
+        }
+    }
+
+    document.getElementById("detalhes").innerHTML += `
+        <h3>${produtos[indicieProd].modelo}</h3>
+        <img src="${produtos[indicieProd].img[0]}" id="frame" width="250" height="250" style="border: 1px solid #000">
+        <div class="miniaturas" id="miniaturas">
+
+        </div>
+    `
+
+    let divMiniaturas = document.getElementById("miniaturas")
+    for(let x of produtos[indicieProd].img){
+        divMiniaturas.innerHTML +=`
+            <img src="${x}" class="mini" width="80" style="border: 1px solid #000">
+        `
+    }
+
+    let minizinhas = document.getElementsByClassName("mini")
+    for(let x of minizinhas){
+        x.addEventListener("mouseover", deslize)
+    }
+}
+
+function deslize(){
+    document.getElementById("frame").src = this.src
+}
+
+buscarDetalhes()
--- Módulo 02/07 - Interação com API/Projeto/index.js
+++ Módulo 02/07 - Interação com API/Projeto/index.js
@@ -1,4 +1,31 @@
 async function listagem(){
-    let 
+    let listagem = await fetch("lista.json")
+    let concecionaria = await listagem.json()
+    let listaDiv = document.getElementById("lista-card")
+
+    for(let x of concecionaria){
+        listaDiv.innerHTML +=`
+            <div class="" data-id="${x.id}">
+                <h3>${x.modelo}</h3>
+                <img src="${x.img[0]}" width="200px" height="200px">
+                <p>${x.descricao}</p>
+                <p>${x.ano}</p>
+                <p>R$ ${x.valorComDesconto}</p>
+                <p>R$ ${x.valorSemDesconto}</p>
+            </div>
+        `
+    }
+
+    let divCards = document.getElementsByClassName("lista-card")
+
+    for(let card of divCards){
+        card.addEventListener("click", clicou)      
+    }
 }
 listagem()
+
+    function clicou(){
+
+        let elementoId = this.getAttribute("data-id")
+        window.location.href = "detalhes.html?produto id=" + elementoId
+    }
