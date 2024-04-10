8dca996e2d2e27180a9e3bb245796e0930b5027a
ReyJulian1533 <gabrielrodrigues2594@gmail.com>
2024-04-10 16:46:10 -0300

    aaaaaaa

--- Módulo 02/07 - Interação com API/Projeto/index.js
+++ Módulo 02/07 - Interação com API/Projeto/index.js
@@ -7,7 +7,7 @@ async function listagem(){
         listaDiv.innerHTML +=`
             <div class="card" data-id="${x.id}">
                 <h2 class="card" style="border: 3px solid goldenrod">${x.modelo}</h3>
-                <img class="card" style="border: 1px solid goldenrod" src="${x.img[0]}" width="200px" height="200px">
+                <img class="card" style="border: 1px solid goldenrod" src="${x.img[0]}" width="150px" height="150px">
                 <h3>${x.descricao}</h3>
                 <h3>${x.descricao2}</h3>
                 <h3>${x.descricao3}</h3>
